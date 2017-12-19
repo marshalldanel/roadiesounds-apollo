f35b5c07f6d770cac04eeb30e521dc037b962917
Marshall Danel <marshalldanel@gmail.com>
2017-12-19 14:12:11 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const schema = require('./schema.js');
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(4000);
+console.log('Listening on 0.0.0.0:4000/graphiql');
